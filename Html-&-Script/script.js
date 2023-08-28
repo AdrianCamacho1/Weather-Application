8c577d48c500604251b405c1abe206c435f346c6
AdrianCamacho1 <Camacho0600@gmail.com>
2023-08-28 11:48:31 -0400

    moved files into repo

---
+++ Html-&-Script/script.js
@@ -0,0 +1,38 @@
+//*task create a weather application
+//GIVEN a weather dashboard with form inputs
+// use css and html to create user dashboard with form inputs like buttons and typebars.
+// *hint use bootstrap , inspect networks for links, use an API to implement weather,
+// with get fetches
+
+//WHEN I search for a city
+// when user types in or click a city button/typebox.
+
+//THEN I am presented with current and future conditions for that city and that city is added to the search history
+// then the user gets returned information about current and future conditions
+// for the selected city and the city is added to the localStorage.
+// *hint use Jquery
+
+
+//WHEN I view current weather conditions for that city
+//THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+//when user views current weather conditions
+
+
+//WHEN I view future weather conditions for that city
+//THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
+
+
+//WHEN I click on a city in the search history
+//THEN I am again presented with current and future conditions for that city
+
+
+//## User Story
+
+//```
+//AS A traveler
+//I WANT to see the weather outlook for multiple cities
+//SO THAT I can plan a trip accordingly
+//```
+
+
